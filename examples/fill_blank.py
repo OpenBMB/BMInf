6c4870d306e2661a05ca8fe0fd9249f414bbc345
zgy <qbjooo@qq.com>
2021-09-23 12:31:52 +0000

    update cpm2 demo

--- examples/fill_blank.py
+++ examples/fill_blank.py
@@ -18,33 +18,12 @@ def fill_blank(cpm2 : bminf.models.CPM2, text):
         text = text.replace(TOKEN_SPAN, "\033[0;32m" + value + "\033[0m", 1)
     print("Output:", text)
 
-input_2 = "天空是蔚蓝色，窗外有"
-
-def generate(model : bminf.models.CPM2, text):
-    with tqdm() as progress_bar:
-        progress_bar.write(text)
-        while True:
-            value = model.generate(
-                input_sentence = text + "<span>", 
-                max_tokens=20,
-                top_n=5,
-                top_p=None,
-                temperature=0.85,
-                frequency_penalty=0,
-                presence_penalty=0
-            )[0]["text"]
-            text += value
-            progress_bar.write(text)
-            progress_bar.update(1)
-            if value.find("<eod>") != -1:
-                break
 
 def main():
     print("Loading model")
     cpm2 = bminf.models.CPM2()
     print("Start")
     fill_blank(cpm2, input_1)
-    generate(cpm2, input_2)
 
 if __name__ == "__main__":
     main()
