1b48d667ce252329d8506b7fc1c1267dd67a485f
zgy <qbjooo@qq.com>
2021-11-26 15:53:03 +0000

    update benchmark

--- benchmark/cpm1/decoder.py
+++ benchmark/cpm1/decoder.py
@@ -9,33 +9,24 @@ def get_args():
     parser = argparse.ArgumentParser(description='CPM2 encoder benchmark')
     parser.add_argument('--device', type=int, default=0, help="Device index")
     parser.add_argument('--batch-size', type=int, default=1, help="Batch size")
-    parser.add_argument('--encode-length', type=int, default=256, help="Encoder input length")
     parser.add_argument('--decode-length', type=int, default=255, help="Decoder output length ( < 256)")
     parser.add_argument('--cases', type=int, default=10, help="Num test cases")
     return parser.parse_args()
 
 def test(
-        model : bminf.models.CPM2, 
-        encoder_output, 
-        mask_enc,
+        model : bminf.models.CPM1, 
         decoder_input,
         decode_length,
-        buf_enc_k,
-        buf_enc_v,
-        buf_dec_k,
-        buf_dec_v
+        buf_k,
+        buf_v,
     ):
     ctx = model._ctx
     for i in range(decode_length):
-        model._model.decode_step(
+        model._model.step(
             ctx,
             decoder_input,
-            encoder_output,
-            mask_enc,
-            buf_dec_k,
-            buf_dec_v,
-            buf_enc_k,
-            buf_enc_v,
+            buf_k,
+            buf_v,
             i
         )
 
@@ -44,30 +35,21 @@ def main():
 
     print("Loading model")
     device_idx = args.device
-    cpm2 = bminf.models.CPM2(device_idx=device_idx)
+    cpm1 = bminf.models.CPM1(device_idx=device_idx)
     
-    ctx = cpm2._ctx
-    encoder_output = ctx.allocate((args.batch_size, cpm2._config.DIM_MODEL, args.encode_length), np.half)
+    ctx = cpm1._ctx
+    decoder_input = ctx.allocate((args.batch_size, cpm1._config.DIM_MODEL), np.half)
 
-    decoder_input = ctx.allocate((args.batch_size, cpm2._config.DIM_MODEL), np.half)
 
-    mask_enc = np.ones((args.batch_size, args.encode_length), np.bool8)
-
-    buf_enc_k = cpm2._model.allocate_decode_buffer(ctx, args.batch_size, args.encode_length)
-    buf_enc_v = cpm2._model.allocate_decode_buffer(ctx, args.batch_size, args.encode_length)
-    buf_dec_k = cpm2._model.allocate_decode_buffer(ctx, args.batch_size, args.decode_length)
-    buf_dec_v = cpm2._model.allocate_decode_buffer(ctx, args.batch_size, args.decode_length)
+    buf_k = cpm1._model.allocate_decode_buffer(ctx, args.batch_size, args.decode_length)
+    buf_v = cpm1._model.allocate_decode_buffer(ctx, args.batch_size, args.decode_length)
 
     test(
-        cpm2, 
-        encoder_output,
-        mask_enc,
+        cpm1,
         decoder_input,
         args.decode_length,
-        buf_enc_k,
-        buf_enc_v,
-        buf_dec_k,
-        buf_dec_v
+        buf_k,
+        buf_v
     )
 
     d = []
@@ -75,15 +57,11 @@ def main():
         cudart.cudaStreamSynchronize(ctx.current_stream)
         st = time.perf_counter()
         test(
-            cpm2, 
-            encoder_output,
-            mask_enc,
+            cpm1,
             decoder_input,
             args.decode_length,
-            buf_enc_k,
-            buf_enc_v,
-            buf_dec_k,
-            buf_dec_v
+            buf_k,
+            buf_v
         )
         cudart.cudaStreamSynchronize(ctx.current_stream)
         st = time.perf_counter() - st
--- benchmark/cpm1/encoder.py
+++ benchmark/cpm1/encoder.py
@@ -14,7 +14,7 @@ def get_args():
     parser.add_argument('--cases', type=int, default=20, help="Num test cases")
     return parser.parse_args()
 
-def test(model : bminf.models.CPM2, input_tensor, mask):
+def test(model : bminf.models.CPM1, input_tensor, mask):
     model._model.encode(
         model._ctx,
         input_tensor,
@@ -25,18 +25,18 @@ def main():
     args = get_args()
 
     print("Loading model")
-    cpm2 = bminf.models.CPM2(device_idx=args.device)
+    cpm1 = bminf.models.CPM1(device_idx=args.device)
 
-    ctx = cpm2._ctx
-    encoder_input = ctx.allocate((args.batch_size, cpm2._config.DIM_MODEL, args.input_length), np.half)
+    ctx = cpm1._ctx
+    encoder_input = ctx.allocate((args.batch_size, cpm1._config.DIM_MODEL, args.input_length), np.half)
     encoder_mask = np.ones((args.batch_size, args.input_length), np.bool8)
-    test(cpm2, encoder_input, encoder_mask)
+    test(cpm1, encoder_input, encoder_mask)
 
     d = []
     for i in tqdm(range(args.cases)):
         cudart.cudaStreamSynchronize(ctx.current_stream)
         st = time.perf_counter()
-        test(cpm2, encoder_input, encoder_mask)
+        test(cpm1, encoder_input, encoder_mask)
         cudart.cudaStreamSynchronize(ctx.current_stream)
         st = time.perf_counter() - st
         d.append(st)
